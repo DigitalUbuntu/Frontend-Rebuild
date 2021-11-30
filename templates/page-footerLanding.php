f1b0ce919cc68550dacbc2b9fbb6b5f260a3e685
adriendiopDU <92724937+adriendiopDU@users.noreply.github.com>
2021-11-30 09:21:57 +0000

    Add files via upload

--- templates/page-footerLanding.php
+++ templates/page-footerLanding.php
@@ -14,13 +14,14 @@ get_header('landing');
             <!-- lan footer logo content -->
             <div class="lan-footer-logo-content">
                 <div class="lan-footer-logo">
-                    <a href="#"> <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/ucao_logo_foot.png"
-                                      alt="Images"></a>
+                    <a href="#"> <img
+                                src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/ucao_logo_foot.png"
+                                alt="Images"></a>
                 </div>
                 <div class="lan-footer-logo-text">
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam nibh aenean quam pharetra, ut
-                        id
-                        purus. Vulputate volutpat nunc rutrum eget semper </p>
+                    <p>L’UCAO/UUZ, un établissement de référence dans le domaine des sciences de gestion, se redéfinit
+                        pour le bénéfice de la population apprenante africaine et devient l’Université Virtuelle de
+                        l’UCAO</p>
                 </div>
             </div>
             <!-- lan footer logo content -->
@@ -28,14 +29,19 @@ get_header('landing');
             <!-- lan footer same content -->
             <div class="lan-footer-same-content">
                 <div class="lan-footer-heading-text">
-                    <h4>Links</h4>
+                    <h4><?php _e('Liens', 'buddyboss-theme-child'); ?></h4>
                 </div>
                 <ul>
-                    <li><a href="#">Home</a></li>
-                    <li><a href="#">Pricing</a></li>
-                    <li><a href="#">Download</a></li>
-                    <li><a href="#">About</a></li>
-                    <li><a href="#">Service</a></li>
+                    <li>
+                        <a href="https://staging-ucao.digitalubuntu.sn/"><?php _e('Acceuil', 'buddyboss-theme-child'); ?></a>
+                    </li>
+                    <li>
+                        <a href="https://staging-ucao.digitalubuntu.sn/environnements-virtuels/"><?php _e('Environnement Virtuel', 'buddyboss-theme-child'); ?></a>
+                    </li>
+                    <li>
+                        <a href="https://staging-ucao.digitalubuntu.sn/about-us/"><?php _e('A Propos', 'buddyboss-theme-child'); ?>
+                        </a></li>
+
                 </ul>
             </div>
             <!-- lan footer same content -->
@@ -43,22 +49,22 @@ get_header('landing');
             <!-- lan footer same content -->
             <div class="lan-footer-same-content">
                 <div class="lan-footer-heading-text">
-                    <h4>Support</h4>
+                    <!--                    <h4>Support</h4>-->
                 </div>
-                <ul>
-                    <li><a href="#">FAQ</a></li>
-                    <li><a href="#">How it</a></li>
-                    <li><a href="#">Features</a></li>
-                    <li><a href="#">Contact</a></li>
-                    <li><a href="#">Reporting</a></li>
-                </ul>
+                <!--  <ul>
+                      <li><a href="#">FAQ</a></li>
+                      <li><a href="#">How it</a></li>
+                      <li><a href="#">Features</a></li>
+                      <li><a href="https://staging-ucao.digitalubuntu.sn/about-us/">Contact</a></li>
+                      <li><a href="#">Reporting</a></li>
+                  </ul>-->
             </div>
             <!-- lan footer same content -->
 
             <!-- lan footer same content -->
             <div class="lan-footer-same-content">
                 <div class="lan-footer-heading-text">
-                    <h4>Contact Us</h4>
+                    <h4><?php _e('Contacter nous', 'buddyboss-theme-child'); ?></h4>
                 </div>
                 <ul>
                     <li><a href="tel:+221338230840">+221338230840</a></li>
@@ -71,21 +77,21 @@ get_header('landing');
 
         <div class="lan-footer-copyright-area">
             <div class="lan-footer-copyright-text">
-                <p>2021 - UCAO LMS propulsé par Digital Ubuntu SA. Dakar, Sénégal</p>
+                <p><?php _e('2021 - UCAO LMS propulsé par Digital Ubuntu SA. Dakar, Sénégal', 'buddyboss-theme-child'); ?></p>
             </div>
             <div class="lan-footer-privecy">
                 <ul>
-                    <li><a href="#">Politique de confidentialité</a></li>
-                    <li><a href="#">Modalités et conditions</a></li>
+                    <li><a href="#"><?php _e('Politique de confidentialité', 'buddyboss-theme-child'); ?></a></li>
+<!--                    <li><a href="#">--><?php //_e('Modalités et conditions', 'buddyboss-theme-child'); ?><!--</a></li>-->
                 </ul>
             </div>
             <div class="lan-footer-social">
                 <ul>
                     <li><a href="#"><i class="fas fa-envelope"></i></a></li>
-                    <li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
-                    <li><a href="#"><i class="fab fa-instagram"></i></a></li>
-                    <li><a href="#"><i class="fab fa-linkedin-in"></i></a></li>
-                    <li><a href="#"><i class="fab fa-twitter"></i></a></li>
+                    <li><a href="https://www.facebook.com/Digitalubuntu/"><i class="fab fa-facebook"  ></i></a></li>
+                    <li><a href="https://www.instagram.com/digitalubuntu/"><i class="fab fa-instagram"></i></a></li>
+                    <li><a href="https://www.linkedin.com/company/digitalubuntu/"><i class="fab fa-linkedin"></i></a></li>
+                    <li><a href="https://twitter.com/digitalubuntu_"><i class="fab fa-twitter"></i></a></li>
                 </ul>
             </div>
         </div>
